0426dac37b1a1d417ef72a39ad1109bd478c77e1
pras.learn02 <pras.learn02@gmail.com>
2023-01-17 06:53:00 +0000

    Updated variables.tf

--- ResourceGroup/variables.tf
+++ ResourceGroup/variables.tf
@@ -1,9 +1,11 @@
 variable "base_name" {
     type = string
     description = "The base of the name for the resource group and storage account"
+    default     = "Terraform-Demo"
 }
 
 variable "location" {
     type = string
     description = "The location for the deployment"
+    default     = "East US"
 }
