fde6a900796ab94685776063154b25a0deda98f8
pras.learn02 <pras.learn02@gmail.com>
2023-01-17 08:47:57 +0000

    Updated main.tf

--- VirtualNetwork/main.tf
+++ VirtualNetwork/main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_virtual_network" "example" {
   name                = "${var.base_name}-VNet"
   address_space       = ["10.0.0.0/16"]
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name  = var.resource_group_name
   location = var.location
 
 }
