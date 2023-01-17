195e8d991f5f3f2073dd563c182274508f51ed97
pras.learn02 <pras.learn02@gmail.com>
2023-01-17 04:26:00 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,27 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.15.00"
+      source = "hashicorp/azurerm"
+      version = "2.88.1"
     }
-  }
-  backend "azurerm" {
-    resource_group_name = var.bkstrgrg
-    storage_account_name = var.bkstrg
-    container_name = var.bkcontainer
-    key = var.bkstrgkey
   }
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    
+  }
+}
+
+module "ResourceGroup" {
+  source = "./ResourceGroup"
+  base_name = "TerraformExample01"
+  location = "West US"
+}
+
+module "StorageAccount" {
+  source = "./StorageAccount"
+  base_name = "TerraformExample01"
+  resource_group_name = module.ResourceGroup.rg_name_out
+  location = "West US"
 }
