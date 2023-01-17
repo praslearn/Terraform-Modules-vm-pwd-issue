a16f7a3dbd739dc8380aade6a9ebd3c71dce2e55
pras.learn02 <pras.learn02@gmail.com>
2023-01-17 04:19:17 +0000

    Added main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.15.00"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name = var.bkstrgrg
+    storage_account_name = var.bkstrg
+    container_name = var.bkcontainer
+    key = var.bkstrgkey
+  }
+}
