a597656b6cccd01d2a51459e0843d750b1baaa56
pras.learn02 <pras.learn02@gmail.com>
2023-01-17 04:27:17 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,17 @@ terraform {
     }
   }
 }
+  backend "azurerm" {
+    resource_group_name = var.bkstrgrg
+    storage_account_name = var.bkstrg
+    container_name = var.bkcontainer
+    key = var.bkstrgkey
+  }
+}
 
+provider "azurerm" {
+  features {}
+}
 provider "azurerm" {
   features {
     
