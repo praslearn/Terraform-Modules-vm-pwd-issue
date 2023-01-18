dbbeed6d4db9168f32380810d81d3a48a9104057
pras.learn02 <pras.learn02@gmail.com>
2023-01-18 03:59:06 +0000

    Updated main.tf

--- Subnet/main.tf
+++ Subnet/main.tf
@@ -5,3 +5,8 @@ resource "azurerm_subnet" "example" {
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.2.0/24"]
 }
+
+ depends_on = [
+azurerm_virtual_network.example.name
+ ]
+}
