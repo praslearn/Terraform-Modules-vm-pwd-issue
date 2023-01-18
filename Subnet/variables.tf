b16f748c901883f56e7da0d7ccc216faa2bbfe72
pras.learn02 <pras.learn02@gmail.com>
2023-01-18 09:49:09 +0000

    Updated variables.tf

--- Subnet/variables.tf
+++ Subnet/variables.tf
@@ -15,4 +15,9 @@ variable "location" {
     description = "The location for the deployment"
     default     = "East US"
 }
+variable "virtual_network_name" {
+    type = string
+    description = "The location for the deployment"
+    default     = "East US"
+}
 
