7ef4dc85b4451d814f5a853e9661bd1f722f488a
pras.learn02 <pras.learn02@gmail.com>
2023-01-18 09:30:52 +0000

    Updated variables.tf

--- Subnet/variables.tf
+++ Subnet/variables.tf
@@ -16,8 +16,3 @@ variable "location" {
     default     = "East US"
 }
 
-variable "azurerm_virtual_network" {
-    type = string
-    description = "The location for the deployment"
-    default     = "East US"
-}
