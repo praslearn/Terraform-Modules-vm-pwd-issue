003b7786f9016e88f51835120b32e64467e51ae5
pras.learn02 <pras.learn02@gmail.com>
2023-01-18 07:42:43 +0000

    Updated main.tf

--- VirtualMachine-Win/main.tf
+++ VirtualMachine-Win/main.tf
@@ -6,7 +6,7 @@ resource "azurerm_windows_virtual_machine" "example" {
   admin_username      = "adminuser"
   admin_password      = "P@$$w0rd1234!"
   network_interface_ids = [
-    azurerm_network_interface.example.id,
+    azurerm_network_interface.example.id
   ]
 
   os_disk {
