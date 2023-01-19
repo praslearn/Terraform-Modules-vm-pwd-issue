880ad6e3bd8e6c435e9aa8f8ec6cd50cd28af4b4
pras.learn02 <pras.learn02@gmail.com>
2023-01-19 06:29:57 +0000

    Updated main.tf

--- Subnet/main.tf
+++ Subnet/main.tf
@@ -1,5 +1,15 @@
 resource "azurerm_subnet" "example" {
-  name                 = "internal"
+  name                 = "example-subnet"
   resource_group_name  = var.resource_group_name
-  address_prefixes     = ["10.0.2.0/24"]
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
+
+  delegation {
+    name = "delegation"
+
+    service_delegation {
+      name    = "Microsoft.ContainerInstance/containerGroups"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"]
+    }
+  }
 }
