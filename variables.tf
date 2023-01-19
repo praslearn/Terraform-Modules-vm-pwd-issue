c5f27fed1f667ab1f7a1622859068bcb40c6a9bf
pras.learn02 <pras.learn02@gmail.com>
2023-01-19 15:00:50 +0000

    Updated variables.tf

--- variables.tf
+++ variables.tf
@@ -3,51 +3,24 @@ variable "base_name" {
     description = "The storage accuont base name"
     default     = "Terraform-Demo"
 }
-
 variable "resource_group_name" {
     type = string
     description = "Name of the resource group"
     default     = "Terraform-Demo-RG"
 }
-
 variable "location" {
     type = string
     description = "The location for the deployment"
     default     = "East US"
-}
-variable "azurerm_network_security_group" {
-   type = string
-  description = "The location for the deployment"
- default     = "East US"
 }
 variable "azurerm_virtual_network" {
     type = string
     description = "The location for the deployment"
     default     = "East US"
 }
-
-
 variable "azurerm_mysql_server" {
     type = string
     description = "The location for the deployment"
     default     = "East US"
 }
-/*
-variable "azurerm_windows_virtual_machine" {
-    type = string
-    description = "The location for the deployment"
-    default     = "East US"
-}
 
-variable "azurerm_network_interface" {
-    type = string
-    description = "The location for the deployment"
-    default     = "East US"
-}
-
-variable "azurerm_subnet" {
-   type = string
-  description = "The location for the deployment"
- default     = "East US"
-}
-*/
