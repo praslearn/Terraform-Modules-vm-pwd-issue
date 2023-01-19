5f47875eb03ef397e1440ade5f1a9522410cb35b
pras.learn02 <pras.learn02@gmail.com>
2023-01-19 06:27:53 +0000

    Updated main.tf

--- Subnet/main.tf
+++ Subnet/main.tf
@@ -1,6 +1,5 @@
 resource "azurerm_subnet" "example" {
   name                 = "internal"
   resource_group_name  = var.resource_group_name
-  location = var.location
   address_prefixes     = ["10.0.2.0/24"]
 }
