9e294645f8fdf886344903d85c47a634f241ed1a
pras.learn02 <pras.learn02@gmail.com>
2023-01-19 09:32:32 +0000

    Updated main.tf

--- Subnet/main.tf
+++ Subnet/main.tf
@@ -1,6 +1,6 @@
 data "azurerm_subnet" "example" {
   name                 = "backend"
-  virtual_network_name =  azurerm_virtual_network.example.name
+  virtual_network_name =  azurerm_virtual_network.example.id
   resource_group_name  = var.resource_group_name
 }
 
