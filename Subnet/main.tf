1c97e63895bace61177762d4ac28d2d60518ff8f
pras.learn02 <pras.learn02@gmail.com>
2023-01-19 10:05:28 +0000

    Deleted Subnet

--- Subnet/main.tf
+++
@@ -1,6 +0,0 @@
-data "azurerm_subnet" "example" {
-  name                 = "backend"
-  virtual_network_name =  azurerm_virtual_network.example.id
-  resource_group_name  = var.resource_group_name
-}
-
