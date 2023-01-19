22f25e4a76ac91b088c0199cc85f00078fdfca3c
pras.learn02 <pras.learn02@gmail.com>
2023-01-19 06:04:58 +0000

    Deleted VirtualNetwork

--- VirtualNetwork/main.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
-  address_space       = ["10.0.0.0/16"]
-  resource_group_name  = var.resource_group_name
-  location = var.location
-}
