106fde2350d7d99489966f5608631a51e9852e14
pras.learn02 <pras.learn02@gmail.com>
2023-01-19 07:01:55 +0000

    Updated main.tf

--- Subnet/main.tf
+++ Subnet/main.tf
@@ -1,7 +1,14 @@
+resource "azurerm_virtual_network" "example" {
+  name                = "example-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = var.resource_group_name
+}
+
 resource "azurerm_subnet" "example" {
   name                 = "example-subnet"
-  resource_group_name  = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.example.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
   address_prefixes     = ["10.0.1.0/24"]
 
   delegation {
