f440f313e8b8674cce37d8d29ce6aa8f3a31088f
pras.learn02 <pras.learn02@gmail.com>
2023-01-19 09:17:21 +0000

    Updated main.tf

--- Subnet/main.tf
+++ Subnet/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_subnet" "example" {
   name                 = "example-subnet"
-  resource_group_name  = azurerm_resource_group.example.name
+  resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.1.0/24"]
 
