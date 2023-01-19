7e1ed57eabf474a2c8aad898d31aa66795347da0
pras.learn02 <pras.learn02@gmail.com>
2023-01-19 13:39:59 +0000

    Updated main.tf

--- Databases/MySQL-DB/main.tf
+++ Databases/MySQL-DB/main.tf
@@ -1,14 +1,17 @@
-resource "azurerm_mssql_database" "test" {
-  name           = "acctest-db-d"
-  server_id      = azurerm_mssql_server.example.id
-  collation      = "SQL_Latin1_General_CP1_CI_AS"
-  license_type   = "LicenseIncluded"
-  max_size_gb    = 4
-  read_scale     = true
-  sku_name       = "S0"
-  zone_redundant = true
+resource "azurerm_mssql_server" "example" {
+  name                         = "example"
+  resource_group_name          = azurerm_resource_group.example.name
+  location                     = azurerm_resource_group.example.location
+  version                      = "12.0"
+  administrator_login          = "4dm1n157r470r"
+  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+}
 
-  tags = {
-    foo = "bar"
-  }
+data "azurerm_mssql_database" "example" {
+  name      = "example-mssql-db"
+  server_id = azurerm_mssql_server.example.id
+}
+
+output "database_id" {
+  value = data.azurerm_mssql_database.example.id
 }
