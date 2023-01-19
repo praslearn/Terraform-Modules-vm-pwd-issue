a8cbcee4ea22b813d5a44b76e50a04a937b8b4d8
pras.learn02 <pras.learn02@gmail.com>
2023-01-19 14:59:09 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -12,72 +12,29 @@ required_providers {
     key = var.bkstrgkey
   }
 }
-
 provider "azurerm" {
   features {}
 }
-
 module "ResourceGroup" {
   source = "./ResourceGroup"
   base_name = "TerraformExample01"
   location = "West US"
 }
-
 module "StorageAccount" {
   source = "./StorageAccount"
   base_name = "TerraformExample01"
   resource_group_name = module.ResourceGroup.rg_name_out
   location = "West US"
 }
-
-
 module "VirtualNetwork" {
   source = "./VirtualNetwork"
   base_name = "TerraformExample01"
   resource_group_name = module.ResourceGroup.rg_name_out
   location = "West US"
 }
-
-
-
 module "MySQL-Server"{
   source = "./Databases/MySQL-Server"
   base_name = "TerraformExample01"
   resource_group_name = module.ResourceGroup.rg_name_out
   location = "West US"
 }
-/*
-module "NetworkSecurityGroup" {
-  source = "./NetworkSecurityGroup"
-  base_name = "TerraformExample01"
-  resource_group_name = module.ResourceGroup.rg_name_out
-  location = "West US"
-}
-
-
-
-module "Subnet" {
-source = "./Subnet"
-base_name = "TerraformExample01"
-resource_group_name = module.ResourceGroup.rg_name_out
-location = "West US"
-virtual_network_name = module.VirtualNetwork.vnet_name_out
-}
-
-module "NetworkInterface" {
-  source = "./NetworkInterface"
-  base_name = "TerraformExample01"
-  resource_group_name = module.ResourceGroup.rg_name_out
-  location = "West US"
-    depends_on = [module.Subnet]
-
-}
-module "VirtualMachine-Win" {
-  source = "./VirtualMachine-Win"
-  base_name = "TerraformExample01"
-  resource_group_name = module.ResourceGroup.rg_name_out
-  location = "West US"
-  depends_on = [module.NetworkInterface]
-
-}
-*/
