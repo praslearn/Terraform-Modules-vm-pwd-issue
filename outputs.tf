b19b109d873ac815a552fec71e480b5a745fc5c9
pras.learn02 <pras.learn02@gmail.com>
2023-01-19 13:20:40 +0000

    Updated outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,19 @@
+output "RgName" {
+  value = module.ResourceGroup.rg_name_out
+}
+
 output "StgActName" {
   value = module.StorageAccount.stg_act_name_out
 }
 
-output "RgName" {
-  value = module.ResourceGroup.rg_name_out
+output "VnetName" {
+  value = module.VirtualNetwork.vnet_name_out
+}
+
+output "MysqlserverName" {
+  value = module.azurerm_mysql_server.mysqlserver_name_out
+}
+
+output "MysqldbName" {
+  value = module.azurerm_mssql_database.mysqldb_name_out
 }
