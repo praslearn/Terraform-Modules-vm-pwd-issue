29ea22a147eaa798dad990c8edafdfccee01e0bd
pras.learn02 <pras.learn02@gmail.com>
2023-01-19 05:53:37 +0000

    Deleted NetworkSecurityGroup

--- NetworkSecurityGroup/main.tf
+++
@@ -1,5 +0,0 @@
-resource "azurerm_network_security_group" "example" {
-  name                = "example-security-group"
-  resource_group_name      = var.resource_group_name
-  location                 = var.location
-}
