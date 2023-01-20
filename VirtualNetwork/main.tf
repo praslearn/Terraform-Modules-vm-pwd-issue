6585c44187038717905bd843714549ba49f58453
pras.learn02 <pras.learn02@gmail.com>
2023-01-20 10:00:07 +0000

    Updated main.tf

--- VirtualNetwork/main.tf
+++ VirtualNetwork/main.tf
@@ -1,3 +1,8 @@
+data "azurerm_network_security_group" "example" {
+  name                = "example"
+  resource_group_name = azurerm_resource_group.example.name
+}
+
 resource "azurerm_virtual_network" "example" {
   name                = "example-network"
   location            = var.location
@@ -9,4 +14,11 @@ resource "azurerm_virtual_network" "example" {
     name           = "subnet1"
     address_prefix = "10.0.1.0/24"
   }
+
+  subnet {
+    name           = "subnet2"
+    address_prefix = "10.0.2.0/24"
+    security_group = azurerm_network_security_group.example.id
+  }
+
 }
