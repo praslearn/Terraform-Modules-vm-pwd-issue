0f7e3fc5352bd937377416947e3d45773f1b2aec
pras.learn02 <pras.learn02@gmail.com>
2023-01-20 13:03:20 +0000

    Updated main.tf

--- Subnet/main.tf
+++ Subnet/main.tf
@@ -1,7 +1,5 @@
 data "azurerm_virtual_network" "example" {
-  name                = "example-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = var.location
+  name                = "example"
   resource_group_name = var.resource_group_name
 }
 
