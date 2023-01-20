22cc219758aad40bed38fad975f9ca0f58e96a7a
pras.learn02 <pras.learn02@gmail.com>
2023-01-20 10:06:05 +0000

    Updated main.tf

--- NetworkInterface/main.tf
+++ NetworkInterface/main.tf
@@ -1,5 +1,22 @@
-data "azurerm_network_interface" "example" {
-  name                = "acctest-nic"
-  resource_group_name = var.resource_group_name
+data "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  resource_group_name  = var.resource_group_name
 }
 
+data "azurerm_subnet" "example" {
+  name                 = "backend"
+  virtual_network_name = "example-network"
+  resource_group_name  = var.resource_group_name
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = var.location
+  resource_group_name  = var.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
