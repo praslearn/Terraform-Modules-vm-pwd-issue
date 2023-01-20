77564b94a34064d60667e549ff71d7e2ecd7f3d0
pras.learn02 <pras.learn02@gmail.com>
2023-01-20 06:22:16 +0000

    Added main.tf

---
+++ NetworkInterface/main.tf
@@ -0,0 +1,11 @@
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
