d6bbe540131a826161029c491239742886b66943
pras.learn02 <pras.learn02@gmail.com>
2023-01-23 10:38:53 +0000

    Updated outputs.tf

--- Subnet/outputs.tf
+++ Subnet/outputs.tf
@@ -1,17 +1,3 @@
-variable "base_name" {
-    type = string
-    description = "The storage accuont base name"
-    default     = "Terraform-Demo"
-}
-
-variable "resource_group_name" {
-    type = string
-    description = "Name of the resource group"
-    default     = "Terraform-Demo-RG"
-}
-
-variable "location" {
-    type = string
-    description = "The location for the deployment"
-    default     = "East US"
+output "subnet_name_out" {
+  value = resource.azurerm_subnet.example.name
 }
