66c2417f4822967501449c05892beb2e89fc625d
pras.learn02 <pras.learn02@gmail.com>
2023-01-23 14:06:56 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ module "NetworkInterface"{
   base_name = "TerraformExample01"
   resource_group_name = module.ResourceGroup.rg_name_out
   location = "West US"
-  depends_on = [module.VirtualNetwork, module.subnet]
+  depends_on = [module.VirtualNetwork, module.Subnet]
 }
 module "ApplicationInsights"{
   source = "./ApplicationInsights"
