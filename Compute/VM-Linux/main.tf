5c708374eafc2a384e48b73fc0ada67a3a1b1b4c
pras.learn02 <pras.learn02@gmail.com>
2023-01-23 12:23:26 +0000

    Updated main.tf

--- Compute/VM-Linux/main.tf
+++ Compute/VM-Linux/main.tf
@@ -16,6 +16,17 @@ data "azurerm_subnet" "example" {
 output "subnet_id" {
   value = data.azurerm_subnet.example.id
 }
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = data.azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
 /*
 resource "azurerm_virtual_network" "example" {
   name                = "example-network"
@@ -31,17 +42,7 @@ resource "azurerm_subnet" "example" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-resource "azurerm_network_interface" "example" {
-  name                = "example-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
 
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.example.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
 */
 resource "azurerm_linux_virtual_machine" "example" {
   name                = "example-machine"
