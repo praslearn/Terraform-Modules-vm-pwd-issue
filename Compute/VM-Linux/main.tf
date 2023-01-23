7203ffd5fad504b9bd1c4d09dffdb87ee9f882ce
pras.learn02 <pras.learn02@gmail.com>
2023-01-23 12:38:42 +0000

    Updated main.tf

--- Compute/VM-Linux/main.tf
+++ Compute/VM-Linux/main.tf
@@ -1,5 +1,5 @@
 data "azurerm_virtual_network" "example" {
-  name                = "example-network"
+  name                = var.virtual_network_name
   resource_group_name = var.resource_group_name
 }
 
