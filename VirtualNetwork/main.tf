be728c9e1beda27ad5dc08dd560b1dc5ca52617a
pras.learn02 <pras.learn02@gmail.com>
2023-01-23 11:21:10 +0000

    Updated main.tf

--- VirtualNetwork/main.tf
+++ VirtualNetwork/main.tf
@@ -10,7 +10,7 @@ resource "azurerm_virtual_network" "example" {
   resource_group_name = var.resource_group_name
   address_space       = ["10.0.0.0/16"]
   dns_servers         = ["10.0.0.4", "10.0.0.5"]
-
+/*
   subnet {
     name           = "subnet1"
     address_prefix = "10.0.1.0/24"
@@ -21,5 +21,5 @@ resource "azurerm_virtual_network" "example" {
     address_prefix = "10.0.2.0/24"
     security_group = azurerm_network_security_group.example.id
   }
-
+*/
 }
