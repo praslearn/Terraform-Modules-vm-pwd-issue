58f8c636fd550b2fe5e94d7bc7761b423ac50ea6
pras.learn02 <pras.learn02@gmail.com>
2023-01-24 04:21:07 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -36,6 +36,7 @@ module "VirtualNetwork" {
   source = "./VirtualNetwork"
   base_name = "TerraformExample01"
   resource_group_name = module.ResourceGroup.rg_name_out
+  depends_on = [module.NSG]
   location = "West US"
 }
 module "NetworkInterface"{
