df02ec22c3f41bfc677138506b53bc328b16d2cb
pras.learn02 <pras.learn02@gmail.com>
2023-01-24 04:20:33 +0000

    Updated main.tf

--- VirtualNetwork/main.tf
+++ VirtualNetwork/main.tf
@@ -9,5 +9,4 @@ resource "azurerm_virtual_network" "example" {
   resource_group_name = var.resource_group_name
   address_space       = ["10.0.0.0/16"]
   dns_servers         = ["10.0.0.4", "10.0.0.5"]
-  depends_on = [data.azurerm_network_security_group.example.name]
 }
