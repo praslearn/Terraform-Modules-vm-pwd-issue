a1889fca546e8a554c499fb70394516772b4f922
pras.learn02 <pras.learn02@gmail.com>
2023-01-24 04:48:26 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -78,7 +78,7 @@ module "VM-Linux"{
   virtual_network_name = module.VirtualNetwork.vnet_name_out
   subnet_name   = module.Subnet.subnet_name_out
   location = "West US"
-    depends_on = [module.VirtualNetwork, module.Subnet,module.NetworkInterface]
+    depends_on = [module.VirtualNetwork, module.Subnet,module.NetworkInterface,module.PublicIP]
 }
 
 /*
