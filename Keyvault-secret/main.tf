f02df6ac57d331760fa45bd3cd2c311e406fb974
pras.learn02 <pras.learn02@gmail.com>
2023-01-24 07:31:44 +0000

    Updated main.tf

--- Keyvault-secret/main.tf
+++ Keyvault-secret/main.tf
@@ -1,8 +1,11 @@
 data "azurerm_key_vault" "example" {
   name                = "examplekeyvault24"
+  resource_group_name = var.resource_group_name
 }
 resource "azurerm_key_vault_secret" "example" {
   name         = "vm-secret"
   value        = "prasath"
   key_vault_id = data.azurerm_key_vault.example.id
+  resource_group_name = var.resource_group_name
+
 }
