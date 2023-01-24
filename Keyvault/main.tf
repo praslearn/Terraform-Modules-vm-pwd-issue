752f4e36617ea5a5439b4a52c76e36b544cc3d44
pras.learn02 <pras.learn02@gmail.com>
2023-01-24 09:08:26 +0000

    Updated main.tf

--- Keyvault/main.tf
+++ Keyvault/main.tf
@@ -1,9 +1,12 @@
+data "azurerm_resource_group" "example" {
+  name = "TerraformExample01"
+}
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_key_vault" "example" {
   name                        = "examplekeyvault24"
   location                    = var.location
-  resource_group_name         = var.resource_group_name
+  resource_group_name        =   azurerm_resource_group.example.name
   enabled_for_disk_encryption = true
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   soft_delete_retention_days  = 7
