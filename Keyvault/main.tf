02f70c4942eea3673b54d2257d17e80b043dc3b0
pras.learn02 <pras.learn02@gmail.com>
2023-01-25 06:46:47 +0000

    Updated main.tf

--- Keyvault/main.tf
+++ Keyvault/main.tf
@@ -5,35 +5,9 @@ resource "random_id" "kvname" {
 }
 #Keyvault Creation
 data "azurerm_client_config" "current" {}
-resource "azurerm_key_vault" "kv1" {
-  depends_on = [ azurerm_resource_group.rg2 ]
-  name                        = random_id.kvname.hex
-  location                    = var.loc1
-  resource_group_name         = var.azure-rg-2
-  enabled_for_disk_encryption = true
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  soft_delete_retention_days  = 7
-  purge_protection_enabled    = false
-  sku_name = "standard"
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
-    key_permissions = [
-      "get",
-    ]
-    secret_permissions = [
-      "get", "backup", "delete", "list", "purge", "recover", "restore", "set",
-    ]
-    storage_permissions = [
-      "get",
-    ]
-  }
-}
-
-/*data "azurerm_client_config" "current" {}
 
 resource "azurerm_key_vault" "example" {
-  name                        = "examplekeyvault24"
+  name                        = random_id.kvname.hex
   location                    = var.location
   resource_group_name        =  var.resource_group_name
   enabled_for_disk_encryption = true
@@ -57,6 +31,34 @@ resource "azurerm_key_vault" "example" {
 
     storage_permissions = [
             "Get",
+    ]
+  }
+}
+
+
+/*
+data "azurerm_client_config" "current" {}
+resource "azurerm_key_vault" "kv1" {
+  depends_on = [ azurerm_resource_group.rg2 ]
+  name                        = random_id.kvname.hex
+  location                    = var.loc1
+  resource_group_name         = var.azure-rg-2
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+  sku_name = "standard"
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+    key_permissions = [
+      "get",
+    ]
+    secret_permissions = [
+      "get", "backup", "delete", "list", "purge", "recover", "restore", "set",
+    ]
+    storage_permissions = [
+      "get",
     ]
   }
 }
