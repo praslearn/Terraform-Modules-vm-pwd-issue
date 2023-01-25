143c857094f0db1acfa21540810ca0e36887677b
pras.learn02 <pras.learn02@gmail.com>
2023-01-25 06:38:30 +0000

    Updated main.tf

--- Keyvault/main.tf
+++ Keyvault/main.tf
@@ -1,4 +1,36 @@
+#Create KeyVault ID
+resource "random_id" "kvname" {
+  byte_length = 5
+  prefix = "keyvault"
+}
+#Keyvault Creation
 data "azurerm_client_config" "current" {}
+resource "azurerm_key_vault" "kv1" {
+  depends_on = [ azurerm_resource_group.rg2 ]
+  name                        = random_id.kvname.hex
+  location                    = var.loc1
+  resource_group_name         = var.azure-rg-2
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+  sku_name = "standard"
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+    key_permissions = [
+      "get",
+    ]
+    secret_permissions = [
+      "get", "backup", "delete", "list", "purge", "recover", "restore", "set",
+    ]
+    storage_permissions = [
+      "get",
+    ]
+  }
+}
+
+/*data "azurerm_client_config" "current" {}
 
 resource "azurerm_key_vault" "example" {
   name                        = "examplekeyvault24"
@@ -28,3 +60,4 @@ resource "azurerm_key_vault" "example" {
     ]
   }
 }
+*/
