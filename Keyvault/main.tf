6b8b845e1d6b1aee7c2ccfcc9e8e8cf50355b7de
pras.learn02 <pras.learn02@gmail.com>
2023-01-26 08:52:15 +0000

    Updated main.tf

--- Keyvault/main.tf
+++ Keyvault/main.tf
@@ -38,13 +38,7 @@ resource "random_password" "vmpassword" {
   length = 20
   special = true
 }
-#Create Key Vault Secret
-resource "azurerm_key_vault_secret" "vmpassword" {
-  name         = "vmpassword"
-  value        = random_password.vmpassword.result
-  key_vault_id = azurerm_key_vault.example.id
-#  depends_on = [ azurerm_key_vault.example ]
-}
+
 /*
 data "azurerm_client_config" "current" {}
 resource "azurerm_key_vault" "kv1" {
