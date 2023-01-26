ac25d1dc87f31dfe8fe57efa90726b280b2e8c32
pras.learn02 <pras.learn02@gmail.com>
2023-01-26 10:03:40 +0000

    Updated main.tf

--- Keyvault-Secret/main.tf
+++ Keyvault-Secret/main.tf
@@ -11,4 +11,5 @@ resource "azurerm_key_vault_secret" "vmpassword" {
   name         = "vmpassword"
   value        = random_password.vmpassword.result
   key_vault_id = data.azurerm_key_vault.example.id
+  depends_on   =[ data.azurerm_key_vault.example]
 }
