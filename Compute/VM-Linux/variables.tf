5a5bd836ad5a05c29c1680b3617d9b8a61dafcfd
pras.learn02 <pras.learn02@gmail.com>
2023-01-26 09:25:10 +0000

    Updated variables.tf

--- Compute/VM-Linux/variables.tf
+++ Compute/VM-Linux/variables.tf
@@ -41,9 +41,5 @@ variable "key_vault_id" {
     description = "The location for the deployment"
     default     = "East US"
 }
-variable "value" {
-    type = string
-    description = "The location for the deployment"
-    default     = "East US"
-}
+
 
