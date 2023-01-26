7e695ec21063268e3b58a9e46da731a203cdff45
pras.learn02 <pras.learn02@gmail.com>
2023-01-26 10:48:57 +0000

    Updated main.tf

--- Keyvault-Secret/main.tf
+++ Keyvault-Secret/main.tf
@@ -1,3 +1,6 @@
+data "azurerm_resource_group" "example" {
+  name = "TerraformExample01"
+}
 data "azurerm_key_vault" "example" {
   name                = "examplekeyvault24"
   resource_group_name = "TerraformExample01"
@@ -11,5 +14,5 @@ resource "azurerm_key_vault_secret" "example" {
   name         = "vmpassword"
   value        = random_password.vmpassword.result
   key_vault_id = data.azurerm_key_vault.example.id
-  depends_on   =[ data.azurerm_key_vault.example]
+  depends_on   =[ data.azurerm_key_vault.example, data.azurerm_resource_group.example]
 }
